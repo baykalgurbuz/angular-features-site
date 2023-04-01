9ec7128b8236d3ffe70efd5ca0e2aa3c2f45ab66
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-04-01 16:18:23 +0300

    Create model and structure of the project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes:Recipe[]=[
+    new Recipe('A Test Recipe','A Test Image','https://www.acouplecooks.com/wp-content/uploads/2019/03/Mushroom-Pasta-007.jpg'),
+    new Recipe('A Test Recipe','A Test Image','https://www.acouplecooks.com/wp-content/uploads/2019/03/Mushroom-Pasta-007.jpg')
+  ];
+  constructor(){}
+
+  ngOnInit(){}
 
 }
